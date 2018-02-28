f68a6191b1b27e47d8c154e00c37c32da55f863f
Alexey Zagalsky <alexeyza@gmail.com>
2018-02-28 15:50:02 -0800

    add a bit more documentation

--- data.go
+++ data.go
@@ -6,10 +6,15 @@ import (
 	"net/http"
 )
 
+// This file includes all the data structures for our game board and game objects
+//
+
 type BSResponse map[string]interface{}
 type StartRequest struct {
 	Game_id int `json: "id"`
 }
+
+// This is the world object
 type MoveRequest struct {
 	Food struct {
 		Data []Point `json:"data"`
@@ -23,10 +28,14 @@ type MoveRequest struct {
 	} `json:"snakes"`
 	You Snake `json:"you"`
 }
+
+// This is a single point with two coordinates: X and Y
 type Point struct {
 	X int `json:"x"`
 	Y int `json:"y"`
 }
+
+// This is the snake object, represents snakes in the game (including ours)
 type Snake struct {
 	Body struct {
 		Data []Point `json:"data"`
@@ -48,7 +57,13 @@ func NewMoveRequest(req *http.Request) (*MoveRequest, error) {
 	err := json.NewDecoder(req.Body).Decode(&decoded)
 	return &decoded, err
 }
-func (snake Snake) Head() Point { return snake.Body.Data[0] }
+
+// This class method returns the head of this snake.
+func (snake Snake) Head() Point {
+	return snake.Body.Data[0]
+}
+
+// This class method checks if this point is outside the map bounds
 func (p *Point) IsOutOfMapBounds(world *MoveRequest) bool {
 	if p.X < 0 || p.Y < 0 {
 		return true
@@ -58,6 +73,8 @@ func (p *Point) IsOutOfMapBounds(world *MoveRequest) bool {
 	}
 	return false
 }
+
+// This class method checks if this point is equal (in values) to the given one
 func (p *Point) Equals(q Point) bool {
 	if p.X == q.X && p.Y == q.Y {
 		return true
@@ -65,6 +82,7 @@ func (p *Point) Equals(q Point) bool {
 	return false
 }
 
+// This class method returns distance between this point and the given point
 func (p1 *Point) distance(p2 Point) float64 {
 	return math.Sqrt(math.Pow(float64(p2.X-p1.X), 2) + math.Pow(float64(p2.Y-p1.Y), 2))
 }
