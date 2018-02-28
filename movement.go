1fc681fab196340aa05b9dffa48e94465b9925bd
Omar Elazhary <omazhary@gmail.com>
2018-02-28 15:37:49 -0800

    Move movement generation to appropriate file

--- movement.go
+++ movement.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"math/rand"
 )
 
@@ -17,15 +15,40 @@ func Strategize(world *MoveRequest) string {
 
 	current_state := NewBoardStateFromMoveRequest(world)
 	moves := current_state.You.GeneratePossibleMoves(current_state)
-	log.Println(fmt.Sprintf("Returned %v possible moves...", len(moves)))
-	log.Println(moves)
 	keys := make([]int, 0, len(moves))
 	for k := range moves {
 		keys = append(keys, k)
 	}
 	random_key := rand.Intn(len(keys))
-	log.Println(keys)
 	next_move := keys[random_key]
-	log.Println(fmt.Printf("Next Move: %v", movement_map[next_move]))
 	return movement_map[next_move]
+}
+
+func (snake Snake) GeneratePossibleMoves(state *BoardState) map[int]Point {
+	moves := make(map[int]Point)
+	var obstructions []Point
+	// Figure out head location
+	head := state.You.Head()
+	// Figure out movement direction(s)
+	moves[1] = Point{head.X, head.Y - 1}
+	moves[2] = Point{head.X + 1, head.Y}
+	moves[3] = Point{head.X, head.Y + 1}
+	moves[4] = Point{head.X - 1, head.Y}
+	// Figure out obstructions in closest squares
+	obstructions = append(obstructions, state.You.Body.Data...)
+	for _, opponent := range state.Snakes {
+		obstructions = append(obstructions, opponent.Head())
+		obstructions = append(obstructions, opponent.Body.Data...)
+	}
+	// Filter out moves based on obstructions
+	for key, value := range moves {
+		if InObstructions(moves[key], obstructions) {
+			delete(moves, key)
+		}
+		// Filter out moves based on border
+		if value.X >= state.BoardWidth || value.X < 0 || value.Y >= state.BoardLength || value.Y < 0 {
+			delete(moves, key)
+		}
+	}
+	return moves
 }
--- state.go
+++ state.go
@@ -1,7 +1,5 @@
 package main
 
-import "log"
-
 type BoardState struct {
 	BoardLength int
 	BoardWidth  int
@@ -19,36 +17,6 @@ func NewBoardStateFromMoveRequest(move_req *MoveRequest) *BoardState {
 		You:         move_req.You,
 	}
 	return &state
-}
-
-func (snake Snake) GeneratePossibleMoves(state *BoardState) map[int]Point {
-	moves := make(map[int]Point)
-	var obstructions []Point
-	// Figure out head location
-	head := state.You.Head()
-	// Figure out movement direction(s)
-	moves[1] = Point{head.X, head.Y - 1}
-	moves[2] = Point{head.X + 1, head.Y}
-	moves[3] = Point{head.X, head.Y + 1}
-	moves[4] = Point{head.X - 1, head.Y}
-	// Figure out obstructions in closest squares
-	obstructions = append(obstructions, state.You.Body.Data...)
-	for _, opponent := range state.Snakes {
-		obstructions = append(obstructions, opponent.Head())
-		obstructions = append(obstructions, opponent.Body.Data...)
-	}
-	log.Println(obstructions)
-	// Filter out moves based on obstructions
-	for key, value := range moves {
-		if InObstructions(moves[key], obstructions) {
-			delete(moves, key)
-		}
-		// Filter out moves based on border
-		if value.X >= state.BoardWidth || value.X < 0 || value.Y >= state.BoardLength || value.Y < 0 {
-			delete(moves, key)
-		}
-	}
-	return moves
 }
 
 func InObstructions(point Point, obstructions []Point) bool {
